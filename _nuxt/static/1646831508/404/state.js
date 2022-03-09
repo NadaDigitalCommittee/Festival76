e5908901294000f070034faec55da9cef0b63629
watcol <watcol@users.noreply.github.com>
2022-03-09 13:11:48 +0000

    deploy: 792dfd6db9b6ed973396b860f1a7d2ca4f63ab38

---
+++ _nuxt/static/1646831508/404/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFrontendSample\u002F_nuxt\u002Fstatic\u002F1646831508",layout:"default",error:a,serverRendered:true,routePath:"\u002F404",config:{_app:{basePath:"\u002FFrontendSample\u002F",assetsPath:"\u002FFrontendSample\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2bc14f16"}}}}(null));
