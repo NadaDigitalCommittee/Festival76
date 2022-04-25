12d529c7b2c4acea2344b684fed83db617d1b45e
motty0533 <motty0533@users.noreply.github.com>
2022-04-25 12:30:46 +0000

    deploy: 0eb6b074a7a3540a8270a0c13a284aaf620cafdb

--- _nuxt/static/1650848273/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fstages\u002Fms","\u002Fnews","\u002Fstage","\u002Fclub","\u002Fblog"]})
---
+++ _nuxt/static/1650889844/club/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFestival76\u002F_nuxt\u002Fstatic\u002F1650889844",layout:"default",error:a,serverRendered:true,routePath:"\u002Fclub",config:{_app:{basePath:"\u002FFestival76\u002F",assetsPath:"\u002FFestival76\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"321dbdb4"}}}}(null));
