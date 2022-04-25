9a80f3c09d190ecaaaecfc590d7ae2c6d5cd77fc
motty0533 <motty0533@users.noreply.github.com>
2022-04-25 00:42:27 +0000

    deploy: e82f5b760aa624ddee03b2aeb005a23ee5f41eb2

--- _nuxt/static/1650812756/stages/ms/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/stages/ms", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1650847343/news/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFestival76\u002F_nuxt\u002Fstatic\u002F1650847343",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnews",config:{_app:{basePath:"\u002FFestival76\u002F",assetsPath:"\u002FFestival76\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f1a3a17f"}}}}(null));
