f7a048929f5496b8d115a5422d7445db4bbad443
watcol <watcol@users.noreply.github.com>
2022-04-27 07:09:46 +0000

    deploy: ad09d6994d532308921108abb138c3b47df00656

---
+++ _nuxt/static/1651043375/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fstage\u002Fmr","\u002Fblog","\u002F","\u002Fclub","\u002Fnews","\u002Fticket","\u002Fstage\u002Fmuscle","\u002Fstage","\u002Fstage\u002Fms"]})
