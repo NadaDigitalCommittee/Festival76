29d2c77c51c2987debba194ab8abe5bf1a0a0526
watcol <watcol@users.noreply.github.com>
2022-04-30 04:50:38 +0000

    deploy: 8ade5947f0edb1b86d9a4cb50c19d726b0c4bb77

---
+++ _nuxt/static/1651294235/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", (function(a){return {data:[{}],fetch:{"News:0":{news:[{description:"「今年度灘校グッズに関するお詫び」を掲載しました。",link:"\u002Fgoods"},{description:"ブログ記事「デザイン課長の中言」を公開しました。",link:a},{description:"ブログ記事「委員長の小言」を公開しました。",link:a},{description:"ホームページを公開しました。",link:"\u002F"}]}},mutations:void 0}}("\u002Fblog")));
