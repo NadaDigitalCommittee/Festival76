238faa4d2158c233f4fc3f2e7bc2cc5f6c8c0477
watcol <watcol@users.noreply.github.com>
2022-04-30 04:52:05 +0000

    deploy: 7e31260c2bc5691a3c8c87d21ca394b58e677c0c

--- _nuxt/static/1651294306/goods/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFestival76\u002F_nuxt\u002Fstatic\u002F1651294306",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgoods",config:{_app:{basePath:"\u002FFestival76\u002F",assetsPath:"\u002FFestival76\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d8fe1f1a"}}}}(null));
---
+++ _nuxt/static/1651294322/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFestival76\u002F_nuxt\u002Fstatic\u002F1651294322",layout:"top",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FFestival76\u002F",assetsPath:"\u002FFestival76\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0534d6b6"}}}}(null));
