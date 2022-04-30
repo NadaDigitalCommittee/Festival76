6199956b1593f605351b516ffe6462ae1433cf00
watcol <potfman@gmail.com>
2022-04-30 13:57:47 +0900

    Fixed the margin.

--- pages/goods.vue
+++ pages/goods.vue
@@ -30,7 +30,7 @@ export default Vue.extend({
 .main {
   width: 90%;
   max-width: 25rem;
-  margin: 1rem auto 1rem;
+  margin: 1rem auto 3rem;
 
   @media screen and (min-width: 800px) {
     width: 80%;
