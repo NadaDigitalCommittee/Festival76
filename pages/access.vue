10dc47a4468a17ca8b3c14af6e038d9c2ee5a7d6
watcol <potfman@gmail.com>
2022-05-01 10:21:46 +0900

    Add access.vue.

---
+++ pages/access.vue
@@ -0,0 +1,157 @@
+<template>
+  <div :class="$style.main">
+    <h1 :class="$style.title">アクセス</h1>
+    <div :class="$style.text">
+      <h2 :class="$style.catch">住所</h2>
+      <p :class="$style.address">
+        <span>〒658-0082</span>
+        <span>兵庫県神戸市東灘区魚崎北町8丁目5番1号</span>
+      </p>
+      <h2 :class="$style.catch">地図</h2>
+      <GoogleMap :class="$style.map" />
+        <h2 :class="$style.catch">駅からのアクセス</h2>
+        <div :class="$style.container">
+          <div :class="$style.way">
+            <h3 :class="$style.caption">JR住吉駅からの道順(PDF)</h3>
+            <Article :class="$style.article" article="e4329f7019514d23" />
+          </div>
+          <ul :class="$style.station">
+            <li>JR住吉駅から徒歩10分</li>
+            <li>阪神魚崎駅から徒歩10分</li>
+            <li>阪急岡本駅から徒歩25分</li>
+          </ul>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import Meta from '@/plugins/meta.js';
+
+export default Vue.extend({
+  name: 'GoodsPage',
+  mixins: [Meta],
+  data() {
+    return {
+      content: {},
+      meta: {
+        title: 'アクセス',
+        description: '灘校へのアクセスです。',
+      },
+    };
+  },
+  async fetch() {
+    this.content = await this.$content('goods').fetch();
+  },
+});
+</script>
+
+<style module lang="scss">
+.main {
+  width: 90%;
+  max-width: 25rem;
+  margin: 1rem auto 3rem;
+
+  @media screen and (min-width: 800px) {
+    width: 80%;
+    max-width: unset;
+  }
+}
+
+.title {
+  width: 100%;
+  font-size: 1.4rem;
+  font-weight: bold;
+  background-color: $white;
+  color: $orange;
+  padding: 0.75rem;
+  margin-bottom: 1rem;
+}
+
+.catch {
+  font-weight: bold;
+  font-size: 1.4em;
+  margin-bottom: 0.5em;
+  position: relative;
+  border-bottom: 2px solid $orange;
+  line-height: 2;
+}
+
+.text {
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 1rem 1rem;
+  background-color: $white;
+}
+
+.map {
+  display: block;
+  height: 22.5rem;
+  margin: 1rem auto;
+  width: 90%;
+}
+
+.address {
+  font-size: 0.875rem;
+  margin-bottom: 1rem;
+
+  span {
+    word-break: keep-all;
+  }
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media screen and (min-width: 800px) {
+    flex-direction: row-reverse;
+    /* align-items: center; */
+  }
+}
+
+.station {
+  font-size: 0.875rem;
+  margin-left: 0.75rem;
+  li {
+    position: relative;
+    margin-left: 1.25em;
+    margin-top: 0.25rem;
+    margin-bottom: 0.25rem;
+  }
+
+  li::before {
+    position: absolute;
+    content: "";
+    top: calc(50% - 0.2em);
+    left: -0.8em;
+    width: 0.4em;
+    height: 0.4em;
+    border-radius: 100%;
+    background-color: $orange;
+  }
+
+  @media screen and (min-width: 800px) {
+    margin-top: 1rem;
+    font-size: 0.9rem;
+  }
+}
+
+.way {
+  width: 10rem;
+  align-self: center;
+  margin: 0 0.75rem 1rem 0.75rem;
+}
+
+.caption {
+  font-size: 0.625rem;
+  font-weight: bold;
+  margin: 0.25rem;
+  text-align: center;
+  color: $orange;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,15 @@
     <img :class="$style.top" src="top.svg">
     <div :class="$style.card">
       <News :limited="true" :class="$style.news" />
+      <div :class="$style.access">
+        <h2 :class="$style.access_header">Access</h2>
+        <GoogleMap :class="$style.map" />
+        <p :class="$style.address">
+          <span>〒658-0082</span>
+          <span>兵庫県神戸市東灘区魚崎北町8丁目5番1号</span>
+        </p>
+        <More :class="$style.more" link="/access" />
+      </div>
     </div>
   </div>
 </template>
@@ -37,7 +46,8 @@ export default Vue.extend({
 
   @media screen and (min-width: 800px) {
     flex-direction: row-reverse;
-    align-items: flex-start;
+    /* flex-direction: row; */
+    /* align-items: flex-start; */
     justify-content: flex-start;
     margin-bottom: 2rem;
     gap: 0;
@@ -49,13 +59,19 @@ export default Vue.extend({
   max-width: 25rem;
 
   @media screen and (min-width: 800px) {
-    width: 45%;
+    width: 50%;
+    max-width: 32rem;
     margin-right: 5%;
   }
 }
 
 .card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   width: 90%;
+  gap: 2rem;
   min-width: 12rem;
   max-width: 25rem;
   min-height: 14.75rem;
@@ -75,8 +91,62 @@ export default Vue.extend({
 
   @media screen and (min-width: 800px) {
     width: 70%;
-    margin-left: auto;
-    margin-right: auto;
   }
+}
+
+.access {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  max-width: 40rem;
+  padding: 0.75rem;
+  width: 100%;
+  height: 100%;
+  min-height: 10rem;
+  background-color: $white;
+
+  @media screen and (min-width: 800px) {
+    width: 70%;
+  }
+}
+
+.access_header {
+  font-family: futura-pt, sans-serif;
+  color: $orange;
+  font-size: 2.25rem;
+  font-weight: bold;
+  margin-left: 0.75rem;
+
+  &::after {
+    content: "";
+    display: block;
+    margin-left: -0.5rem;
+    margin-top: -0.23em;
+    width: 10rem;
+    height: 0.5px;
+    background-color: $gradient-orange;
+  }
+}
+
+.map {
+  display: block;
+  width: 90%;
+  height: 12rem;
+  align-self: center;
+  margin: 0.5rem;
+}
+
+.address {
+  font-size: 0.75rem;
+  text-align: center;
+
+  span {
+    word-break: keep-all;
+  }
+}
+
+.more {
+  align-self: flex-end;
+  margin-right: 0.75rem;
 }
 </style>
