8aa776be4e30bf5e8e7d0f7d11c7014eab0d8362
testuser1111111-nd <testuser1111111-nd@users.noreply.github.com>
2022-05-01 19:27:36 +0000

    deploy: 1a4f2f6a685d0d4fd87f20ae70f5dfc0566c20be

---
+++ _nuxt/static/1651433247/club/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFestival76\u002F_nuxt\u002Fstatic\u002F1651433247",layout:"default",error:a,serverRendered:true,routePath:"\u002Fclub",config:{_app:{basePath:"\u002FFestival76\u002F",assetsPath:"\u002FFestival76\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f637d2b5"}}}}(null));
