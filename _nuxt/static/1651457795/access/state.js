956fd7ad1e703191d6c28be23805e2aacde85975
testuser1111111-nd <testuser1111111-nd@users.noreply.github.com>
2022-05-02 02:16:46 +0000

    deploy: 0677da437f0fd50b58c58e2ad4d4772abaf17baa

---
+++ _nuxt/static/1651457795/access/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFestival76\u002F_nuxt\u002Fstatic\u002F1651457795",layout:"default",error:a,serverRendered:true,routePath:"\u002Faccess",config:{_app:{basePath:"\u002FFestival76\u002F",assetsPath:"\u002FFestival76\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6d2cbb44"}}}}(null));
