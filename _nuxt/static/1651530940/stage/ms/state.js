fee0c6ffb4437020e1b1e05ae80f1f65192d2b5e
watcol <watcol@users.noreply.github.com>
2022-05-02 22:39:55 +0000

    deploy: af067964282a062ee6b7d6b6184dda4681c2ab0a

--- _nuxt/static/1651530940/stage/ms/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFestival76\u002F_nuxt\u002Fstatic\u002F1651530940",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstage\u002Fms",config:{_app:{basePath:"\u002FFestival76\u002F",assetsPath:"\u002FFestival76\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"82a6257d"}}}}(null));
